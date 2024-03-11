55b48a464436a2f713d981cc34e83ff913549c14
Goodluck-creat <nwimochioma15@gmail.com>
2024-03-11 07:51:43 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// function handleintersection(entries,observer){
+//     entries.forEach(element => {
+//         if (entry.isIntersecting){
+//             entry.target.classlist.add('image-animate');
+//             observer.unobserve(entry.target);
+
+
+
+//         }
+//     });
+// }
+// const observer = new
+// IntersectionObserver(handleintersection, {
+//     root:null,
+//     rootMargin:'0px',
+//     threshold:0.5
+// });
+// const imagecontainers  = 
+// document.querySelectorAll('.image-container');
+
+// imagecontainers.forEach(container =>{
+//     observer.observe(container);
+// })
